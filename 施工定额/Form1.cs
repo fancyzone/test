11b6a08ff8ac96d7512cd80bc3f3f8fcff562d28
lihu <35760354+fancyzone@users.noreply.github.com>
2026-05-09 11:56:50 +0800

    Form1更新
    加入注释

--- 施工定额/Form1.cs
+++ 施工定额/Form1.cs
@@ -7,7 +7,7 @@ namespace 施工定额
 {
     public partial class Form1 : Form
     {
-        public class QingdanDingeXiaohaoliang
+        public class QingdanDingeXiaohaoliang //主要类
         {
             public Qingdan qingdan { get; set; } = new Qingdan();
             public List<Dinge> dinge { get; set; } = new List<Dinge>();
